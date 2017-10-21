ee413e7535cc038a5ac712234112be2c806c3dc0
Heinrich Tremblay <het083@g.harvard.edu>
2017-10-21 22:46:40 +0800

    testsuite fix

--- src/components/Text/tests/Text.test.js
+++ src/components/Text/tests/Text.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { shallow } from 'enzyme';
+import Text from ".."
 
 
 describe('Text', () => {
-  it('should work', () => {
-    expect(true).toBe(true);
+  it('show the children in text', () => {
+    const wrapper = shallow(<Text>foo</Text>);
+    expect(wrapper.text()).toBe("foo");
   });
 });
