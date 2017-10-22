8463ac746eaf5e3272b98e1c0c6dfca85ba25104
Heinrich Tremblay <het083@g.harvard.edu>
2017-10-22 14:24:33 +0800

    created boilerplate Icon component

---
+++ src/components/Icon/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import './Icon.css';
+
+class Icon extends Component {
+  static propTypes = {
+  };
+
+  static defaultProps = {
+   };
+
+  render() {
+    return (
+
+    );
+  }
+}
+
+export default Icon;
---
+++ src/components/Icon/tests/Icon.test.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Icon from ".."
+
+
+describe('Icon', () => {
+
+});
