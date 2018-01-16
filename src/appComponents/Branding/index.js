e95bebf529de4a14462c78709675f0368e575b58
Heinrich Tremblay <het083@g.harvard.edu>
2018-01-16 15:42:39 +0800

    added mobile navigation

--- src/appComponents/Branding/index.js
+++ src/appComponents/Branding/index.js
@@ -23,10 +23,12 @@ function Branding({ name, url }) {
       <a href="http://happystack.io" className="branding__logo">
         <img className="branding__logo-image" alt={name} src="/images/logo@2x.png" />
       </a>
-      <Link className="branding__link" to="/"><Text element="h1" bold size="body">{name}</Text></Link>
-      <Text color="main">| </Text>
-      <a className="branding__link" href={url} target="blank"><Text color="main">Repo</Text></a>
-      <div className="clear" />
+      <div className="branding__navigation">
+        <Link className="branding__link" to="/"><Text element="h1" bold size="body">{name}</Text></Link>
+        <Text color="main">| </Text>
+        <a className="branding__link" href={url} target="blank"><Text color="main">Repo</Text></a>
+        <div className="clear" />
+      </div>
     </div>
   );
 }
--- src/appComponents/Navigation/index.js
+++ src/appComponents/Navigation/index.js
@@ -1,8 +1,11 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 import './Navigation.css';
 import Text from '../../components/Text/Text';
+import Button from '../../components/Button/Button';
+import Icon from '../../components/Icon/Icon';
 import Branding from '../Branding';
 
 
@@ -11,26 +14,49 @@ const propTypes = {
 };
 
 
-function Navigation({ routes }) {
-  // sort the routes prop in alphabetic order
-  routes.sort((a, b) => a.name.localeCompare(b.name));
+class Navigation extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { open: false };
+  }
 
-  return (
-    <nav className="navigation">
-      <Branding name="Kit" url="https://github.com/happystacklabs/kit" />
-      <div>
-        <ul className="navigation__ul">
-          {routes.map(route => (
-            <li className="navigation__li" key={route.path}>
-              <NavLink className="navigation__link" to={route.path} activeClassName="navigation__link--active">
-                <Text color="ink-light">{route.name}</Text>
-              </NavLink>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
-  );
+  mobileToggle = () => {
+    this.setState({
+      open: !this.state.open,
+    });
+  };
+
+  render() {
+    // sort the routes prop in alphabetic order
+    this.props.routes.sort((a, b) => a.name.localeCompare(b.name));
+
+    return (
+      <nav className={classNames({ 'navigation--mobile-open': this.state.open }, 'navigation')}>
+        <Branding name="Kit" url="https://github.com/happystacklabs/kit" />
+        <div className="navigation__mobile-menu">
+          <Button className="navigation__mobile-button" plain onClick={this.mobileToggle} >
+            <Icon name={this.state.open ? 'chevron-up' : 'chevron-down'} />
+          </Button>
+        </div>
+        <div className="navigation__content">
+          <ul className="navigation__ul">
+            {this.props.routes.map(route => (
+              <li className="navigation__li" key={route.path}>
+                <NavLink
+                  className="navigation__link"
+                  to={route.path}
+                  activeClassName="navigation__link--active"
+                  onClick={this.mobileToggle}
+                >
+                  <Text color="ink-light">{route.name}</Text>
+                </NavLink>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </nav>
+    );
+  }
 }
 
 
